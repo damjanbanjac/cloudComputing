3967e5e11afe1039de7e7bac4be9fb27ea52432c
damjanbanjac <damjanbanjac14@gmail.com>
2020-12-08 22:00:46 +0100

    added docker-compose file

--- cloudComputing/src/main/java/com/example/cloudComputing/controller/ControllerExample.java
+++ cloudComputing/src/main/java/com/example/cloudComputing/controller/ControllerExample.java
@@ -1,25 +1,25 @@
 package com.example.cloudComputing.controller;
 
-//import com.example.cloudComputing.Service.ServiceExample;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@RequestMapping(value = "")
-//public class ControllerExample {
-//
-//    @Autowired
-//    ServiceExample serviceExample;
-//
-//    @GetMapping(path = "", produces = "application/json")
-//    public ResponseEntity getUser() {
-//
-//        int counter = serviceExample.incrementCounter();
-//        return new ResponseEntity<>(counter, HttpStatus.OK);
-//
-//    }
-//}
+import com.example.cloudComputing.Service.ServiceExample;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "")
+public class ControllerExample {
+
+    @Autowired
+    ServiceExample serviceExample;
+
+    @GetMapping(path = "", produces = "application/json")
+    public ResponseEntity getUser() {
+
+        int counter = serviceExample.incrementCounter();
+        return new ResponseEntity<>(counter, HttpStatus.OK);
+
+    }
+}
--- cloudComputing/src/main/java/com/example/cloudComputing/repository/RepositoryExample.java
+++ cloudComputing/src/main/java/com/example/cloudComputing/repository/RepositoryExample.java
@@ -1,7 +1,7 @@
 package com.example.cloudComputing.repository;
-//
-//import com.example.cloudComputing.model.ModelExample;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface RepositoryExample extends JpaRepository<ModelExample, Long> {
-//}
+
+import com.example.cloudComputing.model.ModelExample;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RepositoryExample extends JpaRepository<ModelExample, Long> {
+}
