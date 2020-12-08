ca1d73680897aa4d3b16738a32473d7696738511
damjanbanjac <damjanbanjac14@gmail.com>
2020-12-08 14:41:07 +0100

    updated Dockerfile

--- cloudComputing/src/main/java/com/example/cloudComputing/Service/ServiceExample.java
+++ cloudComputing/src/main/java/com/example/cloudComputing/Service/ServiceExample.java
@@ -1,29 +1,29 @@
 package com.example.cloudComputing.Service;
 
-import com.example.cloudComputing.model.ModelExample;
-import org.springframework.beans.factory.annotation.Autowired;
-import com.example.cloudComputing.repository.RepositoryExample;
-import org.springframework.stereotype.Service;
-@Service
-public class ServiceExample {
-
-    @Autowired
-    RepositoryExample repository;
-
-    public int incrementCounter() {
-        System.out.println("hello" + System.getenv("PASSWORD"));
-        ModelExample model = repository.findById(new Long(0)).orElse(null);
-
-        if(model == null) {
-            model = new ModelExample();
-            model.setId(new Long(0));
-            model.setCnt(0);
-
-        }
-        model.setCnt(model.getCnt()+1);
-
-        repository.save(model);
-
-        return model.getCnt();
-    }
-}
+//import com.example.cloudComputing.model.ModelExample;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import com.example.cloudComputing.repository.RepositoryExample;
+//import org.springframework.stereotype.Service;
+//@Service
+//public class ServiceExample {
+//
+//    @Autowired
+//    RepositoryExample repository;
+//
+//    public int incrementCounter() {
+//        System.out.println("hello" + System.getenv("PASSWORD"));
+//        ModelExample model = repository.findById(new Long(0)).orElse(null);
+//
+//        if(model == null) {
+//            model = new ModelExample();
+//            model.setId(new Long(0));
+//            model.setCnt(0);
+//
+//        }
+//        model.setCnt(model.getCnt()+1);
+//
+//        repository.save(model);
+//
+//        return model.getCnt();
+//    }
+//}
--- cloudComputing/src/main/java/com/example/cloudComputing/model/ModelExample.java
+++ cloudComputing/src/main/java/com/example/cloudComputing/model/ModelExample.java
@@ -5,19 +5,19 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
-
-
-@Entity(name ="modelExample")
-@Table
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class ModelExample {
-    @Id
-    private Long id;
-
-    @Column(name = "cnt", nullable = false)
-    private int cnt;
-}
+//import javax.persistence.*;
+//
+//
+//@Entity(name ="modelExample")
+//@Table
+//@Getter
+//@Setter
+//@NoArgsConstructor
+//@AllArgsConstructor
+//public class ModelExample {
+//    @Id
+//    private Long id;
+//
+//    @Column(name = "cnt", nullable = false)
+//    private int cnt;
+//}
